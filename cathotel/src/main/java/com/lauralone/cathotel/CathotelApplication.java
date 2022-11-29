acde2d2d5d85e67b77a557d4bafc86cc686f1b4c
Laura Dauner <laura.dauner@gmail.com>
2022-11-29 22:10:40 +0100

    added foundation pom dependencies etc

---
+++ cathotel/src/main/java/com/lauralone/cathotel/CathotelApplication.java
@@ -0,0 +1,12 @@
+package com.lauralone.cathotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CathotelApplication.class, args);
+	}
+}
---
+++ cathotel/src/test/java/com/lauralone/cathotel/CathotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauralone.cathotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
